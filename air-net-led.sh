0380af6533cf60288c80b6f286428b99f31da68f
AlirezaRPi <alireza.kh077@yahoo.com>
2018-01-31 03:08:39 +0330

    Update air-net-led.sh

--- air-net-led.sh
+++ air-net-led.sh
@@ -6,9 +6,9 @@ apFolder=$(ls /sys/class/leds | grep -i airplane)
 while true; do
 		netCvar=$(ping -c 1 4.2.2.4 | grep "64 bytes" | cut -d " " -f 1 2>/dev/null)
 		if [[ netCvar -eq "64" ]]; then
-			echo 255 > /sys/class/leds/$apFolder/brightness
-		else
 			echo 0 > /sys/class/leds/$apFolder/brightness
+		else
+			echo 255 > /sys/class/leds/$apFolder/brightness
 		fi
 		sleep 1
 done
