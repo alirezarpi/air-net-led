a4486a5d3e13f24a6c6a71c1959459ca7583024c
AlirezaRPi <alireza.kh077@yahoo.com>
2018-01-31 13:47:37 +0330

    Update air-net-led.sh

--- air-net-led.sh
+++ air-net-led.sh
@@ -3,6 +3,12 @@
 # Airplane LED - OFF = Internet Access
 # Airplane LED - ON = No Internet Access
 apFolder=$(ls /sys/class/leds | grep -i airplane)
+
+if [[ "$apFolder" == "" ]]; then
+	echo "Your laptop doesn't have Airplane LED"                                          
+	exit 0                                                                                
+fi   
+
 while true; do
 		netCvar=$(ping -c 1 4.2.2.4 | grep "64 bytes" | cut -d " " -f 1 2>/dev/null)
 		if [[ netCvar -eq "64" ]]; then
